8ac4a53be5acd2bbe82160d91f05c37459f58c28
Dmitry Orlov <d.orlov777@gmail.com>
2026-04-26 14:16:15 +0300

    Add Lighthouse CI configuration and update Docker setup

---
+++ .lighthouserc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: [
+        "http://localhost:7860",
+        "http://localhost:7860/booking",
+        "http://localhost:7860/admin",
+      ],
+    },
+    numberOfRuns: 5,
+    settings: {
+      chromeFlags: "--no-sandbox --headless",
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
